387c1aeb298e2b8146b896c2a6f2f8f8aedb9441
kminzi <minji9096@naver.com>
2021-06-28 12:10:46 +0900

    programmers prob_오픈채팅 풀이

--- src/programmers/Solution_openchat.java
+++ src/programmers/Solution_openchat.java
@@ -1,28 +1,44 @@
 package programmers;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+
 public class Solution_openchat {
 
     static String[] solution(String[] record) {
-        String[] answer = {};
+        HashMap<String, String> idmap = new HashMap<>();
+        ArrayList<String> tmp = new ArrayList<>();
 
-        for (int i=0; i<record.length; i++){
+        //hashmap을 이용하여 아이디-닉네임 저장
+        for (int i = 0; i < record.length; i++) {
             String[] message = record[i].split(" ");
-            String command = message[0];
-            String id = message[1];
-            String name = message[2];
-
-            if(command.equals("Change")){
-
+            if (message[0].equals("Enter") || message[0].equals("Change")) {
+                idmap.put(message[1], message[2]);
             }
-
-
         }
 
+        //출력문 생성
+        for (int i = 0; i < record.length; i++) {
+            String[] message = record[i].split(" ");
+            if (message[0].equals("Enter")) {
+                tmp.add(idmap.get(message[1]) + "님이 들어왔습니다.");
+            } else if (message[0].equals("Leave")) {
+                tmp.add(idmap.get(message[1]) + "님이 나갔습니다.");
+            }
+        }
+
+        //arraylist->array 변환
+        String[] answer = new String[tmp.size()];
+        for (int i = 0; i < answer.length; i++) {
+            answer[i] = tmp.get(i);
+        }
         return answer;
     }
 
     public static void main(String[] args) {
-        String[] test1 = {"Enter uid1234 Muzi", "Enter uid4567 Prodo","Leave uid1234","Enter uid1234 Prodo","Change uid4567 Ryan"};
+        String[] test1 = {"Enter uid1234 Muzi", "Enter uid4567 Prodo", "Leave uid1234", "Enter uid1234 Prodo", "Change uid4567 Ryan"};
 
+        System.out.println("my ans : " + Arrays.toString(solution(test1)) + " ans : [\"Prodo님이 들어왔습니다.\", \"Ryan님이 들어왔습니다.\", \"Prodo님이 나갔습니다.\", \"Prodo님이 들어왔습니다.\"]");
     }
 }
